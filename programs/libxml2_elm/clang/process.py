6ced351b5996c2b2c98c24f3c265a0c73391f0f3
Chuyang Chen <chuyangchen2018@outlook.com>
2024-08-18 19:03:46 -0400

    Fix a bug

--- programs/libxml2_elm/clang/process.py
+++ programs/libxml2_elm/clang/process.py
@@ -1,5 +1,5 @@
 with open('/src/build.sh') as build_f:
-    build_script = build_f.read()
-build_script = build_script.replace('make', 'bear make')
+    build_script = build_f.readlines()
+build_script = [l.replace('make', 'bear make') for l in build_script[:32]]
 with open('/src/build.sh', 'w') as build_f:
-    build_f.write(build_script)
+    build_f.writelines(build_script)
