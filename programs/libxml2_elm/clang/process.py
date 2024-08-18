98d0d303bfdc49390607ecde3a8d0b6c1816a484
Chuyang Chen <chuyangchen2018@outlook.com>
2024-08-18 19:31:37 -0400

    Fix bugs

--- programs/libxml2_elm/clang/process.py
+++ programs/libxml2_elm/clang/process.py
@@ -1,11 +1,12 @@
 with open('/src/build.sh') as build_f:
     build_script = build_f.readlines()
 new_build_script = []
-for line in build_script:
+for line in build_script[:31]:
     line = line.replace('make', 'bear make') \
-               .replace('autogen.sh', 'configure')
+               .replace('autogen.sh', 'configure') \
+               .replace('--disable-shared', '--enable-shared=no')
     if line.strip().startswith('--without'):
-        line = line.replace('--without', '--with') + '=off'
+        line = line.replace('--without', '--with').replace(' \\', '=off \\')
     new_build_script.append(line)
 with open('/src/build.sh', 'w') as build_f:
     build_f.writelines(new_build_script)
